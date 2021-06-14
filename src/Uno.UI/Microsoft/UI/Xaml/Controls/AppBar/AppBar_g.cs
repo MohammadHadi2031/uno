2987191187c11fab1b3970c95bc81e5612c9fd22
Steve Bilogan <steve.bilogan@gmail.com>
2021-06-14 16:46:10 -0400

    feat: Implement CommandBar and AppBar

---
+++ src/Uno.UI/Microsoft/UI/Xaml/Controls/AppBar/AppBar_g.cs
@@ -0,0 +1,16 @@
+﻿#nullable enable
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.UI.Xaml.Controls
+{
+	partial class AppBar
+	{
+		public event EventHandler<object>? Closed;
+		public event EventHandler<object>? Closing;
+		public event EventHandler<object>? Opened;
+		public event EventHandler<object>? Opening;
+	}
+}
